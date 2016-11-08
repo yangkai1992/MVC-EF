9c1606a9c49a7586ee3ac2fac844c196d9bc3c19
yangkai <451201807@qq.com>
2016-11-08 18:40:35 +0800

    提交

---
+++ Repository/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public sealed class DataContext:DbContext,IDbContext
+    {
+
+        public DataContext(string connectionString)
+            : base(connectionString)
+        { 
+        
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}
