8879d8f6f00d0708a4d01b1cde51157947e3df32
yangkai <451201807@qq.com>
2016-12-23 16:23:38 +0800

    登录 注册UI完成

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace Web
                         "~/Scripts/Plugin/bootstrap/js/bootstrap.js"
                         , "~/Scripts/Plugin/dist/js/app.js"
                         , "~/Scripts/Plugin/plugins/jQuery/jquery-2.2.3.min.js"
-                        , "~/Scripts/Plugin/plugins/iCkeck/icheck.js"));
+                        , "~/Scripts/Plugin/plugins/iCheck/icheck.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/common.css"
@@ -31,7 +31,7 @@ namespace Web
                       , "~/Scripts/Plugin/bootstrap/ionicons-2.0.1.min.css"
                       , "~/Scripts/Plugin/dist/css/AdminLTE.css"
                       , "~/Scripts/Plugin/dist/css/skins/_all-skins.css"
-                      , "~/Scripts/Plugin/plugins/iCkeck/square/blue.css"));
+                      , "~/Scripts/Plugin/plugins/iCheck/square/blue.css"));
         }
     }
 }
